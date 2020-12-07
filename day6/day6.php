825f726f56a79181df1e9ee0835825672c12fed7
WorldWillWeb <contact@william-gerard.fr>
2020-12-07 14:47:46 +0100

    day6 - part2

--- day6/day6.php
+++ day6/day6.php
@@ -7,23 +7,45 @@ $data = explode("\n\n", $input);
 
 $alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
 
-foreach ($data as $key => $value) {
-    $group[$key] = $value;
+$nbLine = [];
+
+foreach ($data as $groupkey => $value) {
+    $group[$groupkey] = $value;
 }
 
 foreach ($group as $key => $questionsYes) {
+    $listQuestionYes = explode("\n", $questionsYes);
+    $nbLine[$key] = count($listQuestionYes);
+    
     foreach ($alphabet as $letter) {
         $question[$letter] = false;
     }
     $questionsYes = str_split($questionsYes);
+    $groupYes = [];
+        
     foreach ($questionsYes as $yes) {
+        
         if (key_exists($yes,$question)) {
             $question[$yes] = true;
+        }
+
+        if (!in_array($yes,$groupYes) && key_exists($yes,$question))  {
+            $groupYes[$key][] = $yes;
         }
     }
     foreach ($question as $value)  {
         if ($value === true) {
             $result1++;
+        }
+    }
+
+    $listValues = array_count_values($groupYes[$key]);
+
+      
+    
+    foreach ($listValues as $response) {
+        if ($response == $nbLine[$key]) {
+            $result2++;
         }
     }
 }
@@ -33,5 +55,6 @@ echo '<h1> Etape 1 </h1>';
 echo $result1;
 
 echo '<h1> Etape 2 </h1>';
+echo $result2;
 
 
